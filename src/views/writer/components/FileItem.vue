5741794bf3e71c1f5cbafeba17d08448ba683011
ZJX <1115898584@qq.com>
2024-11-20 22:31:19 +0800

    bug

--- src/views/writer/components/FileItem.vue
+++ src/views/writer/components/FileItem.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { type FileInfo } from "@/type";
 interface Props {
   file: FileInfo;
 }
+
 const props = defineProps<Props>();
 const percentage = ref(0);
+const size = computed(() => {
+  return props.file.size < 1024
+    ? `${props.file.size} B`
+    : props.file.size < 1048576
+    ? `${(props.file.size / 1024).toFixed(2)} KB`
+    : props.file.size < 1073741824
+    ? `${(props.file.size / 1048576).toFixed(2)} MB`
+    : `${(props.file.size / 1073741824).toFixed(2)} GB`;
+});
 onMounted(() => {
   setInterval(() => {
     if (props.file.id) {
@@ -21,6 +31,7 @@ onMounted(() => {
   <div class="file-item">
     <div style="position: relative" v-if="!file.id">
       <el-progress
+        :show-text="false"
         style="position: relative; top: 10px"
         :text-inside="true"
         :percentage="percentage"
@@ -35,7 +46,7 @@ onMounted(() => {
         {{ file.type.split("/").pop() }}
       </div>
       <div class="size" :style="{ flex: 1 }">
-        {{ (file.size / 1024).toFixed(0) }} KB
+        {{ size }}
       </div>
     </div>
   </div>
@@ -53,6 +64,21 @@ onMounted(() => {
     letter-spacing: 1px;
     gap: 10px;
     .name {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .date {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .type {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .size {
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
